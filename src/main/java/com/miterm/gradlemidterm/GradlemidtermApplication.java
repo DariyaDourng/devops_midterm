ff90754df1bb727b3f4e429a9f0b4d54bd19f822
DariyaDourng <dariyadourng@gmail.com>
2024-03-29 14:57:15 +0700

    spring boot project

---
+++ src/main/java/com/miterm/gradlemidterm/GradlemidtermApplication.java
@@ -0,0 +1,13 @@
+package com.miterm.gradlemidterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradlemidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradlemidtermApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miterm/gradlemidterm/GradlemidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miterm.gradlemidterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradlemidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
